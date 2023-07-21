8d96d0d87b384ac70f1cbbefeaaee0326ea49d6d
Brian Schubert <schubert.b@northeastern.edu>
2023-07-20 21:30:26 -0400

    Add resampling test suite

--- tests/test_splib07.py
+++ tests/test_splib07.py
@@ -6,6 +6,8 @@ warnings.filterwarnings("ignore", category=DeprecationWarning, module="nptyping"
 import pathlib
 from typing import Final
 
+import numpy as np
+import numpy.testing
 import pytest
 
 import splib07
@@ -25,6 +27,31 @@ def library() -> splib07.Splib07:
 
 
 @skip_if_no_archive
-def test_scan_archive(library):
+def test_scan_archive(library) -> None:
     assert len(library.list_spectra()) == 2457
     assert len(library.list_resamplings()) == 29
+
+
+@skip_if_no_archive
+def test_experimental_resample(library) -> None:
+    # spectrum_name = random.choice(library.list_spectra())
+    spectrum_name = "Seawater_Coast_Chl_SW1"
+
+    for resampling in library.list_resamplings():
+        print(f"testing {resampling}")
+
+        expected_spectrum = library.load(spectrum_name, resample=resampling)
+        resampled_spectrum = library.load(
+            spectrum_name,
+            resample=(expected_spectrum.wavelengths, expected_spectrum.fwhm),
+        )
+        nan_mask = np.isnan(expected_spectrum.spectrum) | np.isnan(
+            resampled_spectrum.spectrum
+        )
+
+        np.testing.assert_allclose(
+            resampled_spectrum.spectrum[~nan_mask],
+            expected_spectrum.spectrum[~nan_mask],
+            atol=0.01,
+            rtol=0.01,
+        )
