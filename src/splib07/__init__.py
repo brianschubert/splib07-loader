09bf4f9b056a70184849171764812d7ad00580c2
Brian Schubert <schubert.b@northeastern.edu>
2023-08-07 14:40:34 -0400

    Add module docstring

--- src/splib07/__init__.py
+++ src/splib07/__init__.py
@@ -1,3 +1,9 @@
+"""
+Tools for loading spectra from a local archive of the `USGS Spectral Library Version 7`_.
+
+.. _USGS Spectral Library Version 7: https://pubs.er.usgs.gov/publication/ds1035
+"""
+
 import importlib.metadata
 
 from ._loader import Spectrum, Splib07
