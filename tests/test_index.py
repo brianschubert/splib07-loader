881fd9ef6373bd2b51080a692d0c4b17aca26244
Brian Schubert <schubert.b@northeastern.edu>
2024-01-02 20:18:02 -0500

    Update test imports

--- tests/test_index.py
+++ tests/test_index.py
@@ -4,36 +4,36 @@ from typing import Final
 
 import pytest
 
-import splib07._index as splib07_index
+import splib07
 
 _EXPECTED_CHAPTER_LENGTHS: Final = (1275, 208, 11, 23, 359, 289, 285)
 
 
 @pytest.fixture(scope="module")
-def generated_index(library_path) -> splib07_index.Splib07Index:
-    return splib07_index.Splib07Index.generate_index(library_path)
+def generated_index(library_path) -> splib07._index.Splib07Index:
+    return splib07._index.Splib07Index.generate_index(library_path)
 
 
 def test_resolve_zip_path_identifies_zip(library_path) -> None:
-    resolved_path = splib07_index._resolve_zip_path(library_path)
+    resolved_path = splib07._util.resolve_zip_path(library_path)
 
     expected_type = zipfile.Path if library_path.suffix == ".zip" else pathlib.Path
     assert isinstance(resolved_path, expected_type)
 
 
 def test_read_index_finds_all_sampling_datatables(library_path) -> None:
-    sampling_datatables = splib07_index._read_toc_sampling_paths(
-        splib07_index._resolve_zip_path(library_path)
+    sampling_datatables = splib07._index._read_toc_sampling_paths(
+        splib07._util.resolve_zip_path(library_path)
         / "indexes"
         / "table_of_contents.html"
     )
 
-    assert len(sampling_datatables) == len(splib07_index.Sampling)
+    assert len(sampling_datatables) == len(splib07._index.Sampling)
 
 
 def test_generate_index_finds_all_entries(generated_index) -> None:
     # Check that there is an index for every available sampling.
-    assert len(generated_index._sampling_indices) == len(splib07_index.Sampling)
+    assert len(generated_index._sampling_indices) == len(splib07._index.Sampling)
 
     for sampling_index in generated_index._sampling_indices.values():
         chapter_lengths = tuple(map(len, sampling_index))
