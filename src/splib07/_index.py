dad53b2a25b41d5dec744538dec6697dbaaa1155
Brian Schubert <schubert.b@northeastern.edu>
2024-01-15 13:55:45 -0500

    Add license headers

--- src/splib07/_index.py
+++ src/splib07/_index.py
@@ -1,8 +1,22 @@
+# Copyright (C) 2023 Brian Schubert.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import annotations
 
 import collections
-import enum
 import functools
+import importlib.resources
 import itertools
 import lzma
 import pathlib
@@ -17,14 +31,12 @@ from typing import (
     NamedTuple,
     overload,
 )
-import importlib.resources
 
 import bs4
 from typing_extensions import Self, TypeAlias
 
 from splib07._common import Chapter, Sampling, SpectrumIdentifier
 from splib07._util import PathLike, VirtualPath, resolve_zip_path
-
 
 _ChapterIndex: TypeAlias = MutableMapping[SpectrumIdentifier, "_SpectrumEntry"]
 """Mapping of all spectra contained in a chapter."""
